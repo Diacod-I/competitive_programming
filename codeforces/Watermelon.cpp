18ab59cbe7299f375cfe6e02097084e6b6275e0c
Advith Krishnan <advithkrishnan@Adviths-MacBook-Air.local>
2025-07-29 15:26:50 +0530

    4A-Watermelon (Codeforces Beta Round 4 Div. 2)

---
+++ codeforces/Watermelon.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+    int w;
+    std::cin >> w;
+
+    //Check for w being odd
+    if(w%2 || w==2){
+        std::cout<<"NO";
+        return 0;
+    }
+
+    //Split into halves, +1 for one half and -1 for another half and check for odd
+    else if(w%2==1 && (((w/2)+1)%2 || ((w/2)-1)%2)) {
+        std::cout<<"NO";
+        return 0;
+    }
+
+    //Otherwise, YES
+    else {
+        std::cout<<"YES";
+    }
+    return 0;
+}
